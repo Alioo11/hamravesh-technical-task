cab103e4671e2f2789e9ea4829931ac769c07a1b
Alioo11 <110mors110@gmail.com>
2024-09-09 14:55:17 +0330

    fix: fix dummy api response issue

--- pages/api/task.ts
+++ pages/api/task.ts
@@ -5,49 +5,53 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 const tasksFilePath = path.join(process.cwd(), "data/tasks.json");
 
-const handleGetTasks = (req: NextApiRequest, res: NextApiResponse<Array<ITask>>) => {
+type errorResponse = {
+  message: string;
+};
+
+const handleGetTasks = (req: NextApiRequest, res: NextApiResponse<Array<ITask> | errorResponse>) => {
   try {
-    const search = req.query.search as string | "";
+    const search = req.query.search as string || "";
     const tasksJSON = fs.readFileSync(tasksFilePath, "utf8");
     const tasks = JSON.parse(tasksJSON) as Array<ITask>;
+    const filteredTasks = tasks.filter((task) => task.title.toLowerCase().includes(search.toLowerCase()));
 
-    const filteredTasks = tasks.filter((task) => task.title.includes(search));
-    res.send(filteredTasks);
+    res.status(200).json(filteredTasks);
   } catch (error) {
     console.log(error);
-    res.status(500);
+    res.status(500).json({ message: "something wen't wrong" });
   }
 };
 
-const handleCreateTasks = (req: NextApiRequest, res: NextApiResponse<Array<ITask>>) => {
+const handleCreateTasks = (req: NextApiRequest, res: NextApiResponse<ITask | errorResponse>) => {
   try {
-    const body = req.body;
+    const bodyData = req.body.data;
     const tasksJSON = fs.readFileSync(tasksFilePath, "utf8");
     const tasks = JSON.parse(tasksJSON) as Array<ITask>;
 
     const newTask: ITask = {
       id: Math.floor(Math.random() * 1000), // dummy id and userId
       userId: Math.floor(Math.random() * 1000),
-      title: body.title,
-      status: body.status,
+      title: bodyData.title,
+      status: bodyData.status,
     };
 
     fs.writeFileSync(tasksFilePath, JSON.stringify([...tasks, newTask]), "utf8");
 
-    res.status(201);
+    res.status(201).json(newTask);
   } catch (error) {
     console.log(error);
-    res.status(500);
+    res.status(500).json({message:"something wen't wrong"});
   }
 };
 
 /**
  * @param {NextApiRequest} req
- * @param {NextApiResponse<Array<Transaction>> }res
+ * @param {NextApiResponse<Array<ITask>> }res
  *
  * @description dummy api to return mock todo data :)
  */
-export default function handler(req: NextApiRequest, res: NextApiResponse<Array<ITask>>) {
-  if (req.method === "GET") return handleGetTasks(req, res);
-  if (req.method === "POST") return handleCreateTasks(req, res);
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") handleGetTasks(req, res);
+  else if (req.method === "POST") handleCreateTasks(req, res);
 }
