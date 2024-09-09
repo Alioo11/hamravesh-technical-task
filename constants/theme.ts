2633e43fb343266f837a2fe73b40a3d8392e2b98
Alioo11 <110mors110@gmail.com>
2024-09-09 20:43:44 +0330

    feat: update theme config

--- constants/theme.ts
+++ constants/theme.ts
@@ -1,33 +1,33 @@
 import type { ThemeConfig } from "antd";
 
-// Light theme configuration
 export const lightTheme: ThemeConfig = {
   token: {
-    colorPrimary: "#1890ff",        // Primary color
-    colorText: "#000000",           // Text color
-    colorLink: "#1890ff",           // Link color
-    colorBgBase: "#ffffff",         // Base background color
-    colorBgContainer: "#f0f2f5",    // Background color for containers
-    colorBgLayout: "#ffffff",       // Background color for layout
-    colorBorder: "#d9d9d9",         // Border color
-    colorBorderSecondary: "#f0f0f0",// Secondary border color
-    colorTextSecondary: "#595959",  // Secondary text color
-    colorTextPlaceholder: "#bfbfbf" // Placeholder text color
+    colorPrimary: "#1890ff",
+    colorText: "#000000",
+    colorLink: "#1890ff",
+    colorBgBase: "#ffffff",
+    colorBgContainer: "#f0f2f5",
+    colorBgLayout: "#ffffff",
+    colorBorder: "#d9d9d9",
+    colorBorderSecondary: "#f0f0f0",
+    colorTextSecondary: "#595959",
+    colorTextPlaceholder: "#bfbfbf",
   },
 };
 
-// Dark theme configuration
 export const darkTheme: ThemeConfig = {
   token: {
-    colorPrimary: "#1890ff",        // Primary color
-    colorText: "#ffffff",           // Text color
-    colorLink: "#1890ff",           // Link color
-    colorBgBase: "#001529",         // Base background color
-    colorBgContainer: "#002140",    // Background color for containers
-    colorBgLayout: "#001529",       // Background color for layout
-    colorBorder: "#434343",         // Border color
-    colorBorderSecondary: "#2f2f2f",// Secondary border color
-    colorTextSecondary: "#d9d9d9",  // Secondary text color
-    colorTextPlaceholder: "#8c8c8c" // Placeholder text color
+    colorPrimary: "#1890ff",
+    colorText: "#ffffff",
+    colorLink: "#1890ff",
+    colorBgBase: "#001529",
+    colorBgContainer: "#002140",
+    controlItemBgActive: "#1890dd",
+    colorBgContainerDisabled: "#888888",
+    colorBgLayout: "#001529",
+    colorBorder: "#434343",
+    colorBorderSecondary: "#2f2f2f",
+    colorTextSecondary: "#d9d9d9",
+    colorTextPlaceholder: "#8c8c8c",
   },
 };
